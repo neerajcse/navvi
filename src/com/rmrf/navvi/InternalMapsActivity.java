cccc3351265407a580e9ce45cab3143ae01f94d3
Neeraj Kumar <neerajpkumar@gmail.com>
2014-02-23 02:15:17 -0800

    Adding image viewere with touch gestures

--- src/com/rmrf/navvi/InternalMapsActivity.java
+++ src/com/rmrf/navvi/InternalMapsActivity.java
@@ -1,18 +1,30 @@
 package com.rmrf.navvi;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.EditText;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,7 +51,14 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
 
 	private String locationName;
 	private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
+	
+	private IBeacon currentLocationBeacon;
+	private IBeacon lastLocationBeacon;
 
+	private static int displayWidth;
+	private static int displayHeight;
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -53,9 +72,18 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
 		locationName = bundle.getString("name");
 		this.setTitle("Map for " + locationName);
 
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		final LinearLayout controlsView = (LinearLayout)findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
+		
+		
+		
+		Display display = ((WindowManager)
+                getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
 
+		displayWidth = display.getWidth();             
+		displayHeight = display.getHeight();
+
+		
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
 		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
@@ -118,7 +146,10 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
 		// while interacting with the UI.
 		findViewById(R.id.imageView1).setOnTouchListener(
 				mDelayHideTouchListener);
-		new RetreiveImageTask().execute(locationName);
+		controlsView.removeAllViews();
+		controlsView.addView(new SampleView(getApplicationContext()));
+		//new RetreiveImageTask().execute(locationName);
+		
 	}
 
 
@@ -165,6 +196,9 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
 		}
 	};
 
+	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 	/**
 	 * Schedules a call to hide() in [delay] milliseconds, canceling any
 	 * previously scheduled calls.
@@ -204,11 +238,13 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
         super.onDestroy();
         iBeaconManager.unBind(this);
     }
+	
     @Override 
     protected void onPause() {
     	super.onPause();
     	if (iBeaconManager.isBound(this)) iBeaconManager.setBackgroundMode(this, true);    		
     }
+    
     @Override 
     protected void onResume() {
     	super.onResume();
@@ -232,9 +268,23 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
 		iBeaconManager.setRangeNotifier(new RangeNotifier() {
             @Override 
             public void didRangeBeaconsInRegion(Collection<IBeacon> iBeacons, Region region) {
-                if (iBeacons.size() > 0) {
-                	logToDisplay("The first iBeacon I see is about "+iBeacons.iterator().next().getAccuracy()+" meters away.");            	
+                
+            	if (iBeacons.size() > 0) {
+                	if (iBeacons.size() == 1) {
+                		currentLocationBeacon = iBeacons.iterator().next();
+                	} else {
+                		currentLocationBeacon = getNearestBeacon(iBeacons);
+                	}
+                	logToDisplay(
+                			String.format("Total Beacons %d\n Nearst Beacons:%s\nNearst beacon id:%s", 
+                					iBeacons.size(),
+                					currentLocationBeacon.getProximityUuid(),
+                					currentLocationBeacon.getAccuracy())
+                			);   	
+                } else {
+                	currentLocationBeacon = null;
                 }
+                
             }
 
             });
@@ -244,5 +294,108 @@ public class InternalMapsActivity extends Activity implements IBeaconConsumer{
             } catch (RemoteException e) {   }
 
     }
+	
+	private IBeacon getNearestBeacon(Collection<IBeacon> iBeacons) {
+		
+		Comparator<IBeacon> comparator = new Comparator<IBeacon>() {
+
+			@Override
+			public int compare(IBeacon lhs, IBeacon rhs) {
+				if (lhs.getAccuracy() == rhs.getAccuracy()) return 0;	
+				return lhs.getAccuracy() < rhs.getAccuracy() ? -1 : 1;
+			}
+		};
+		List<IBeacon> beaconList = new ArrayList<IBeacon>(iBeacons);
+		Collections.sort(beaconList, comparator);
+		return beaconList.get(0);
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	/*******************Custom View for Image scroller ******************/
+	 private static class SampleView extends View {
+         private static Bitmap bmLargeImage; //bitmap large enough to be scrolled
+         private static Rect displayRect = null; //rect we display to
+         private Rect scrollRect = null; //rect we scroll over our bitmap with
+         private int scrollRectX = 0; //current left location of scroll rect
+         private int scrollRectY = 0; //current top location of scroll rect
+         private float scrollByX = 0; //x amount to scroll by
+         private float scrollByY = 0; //y amount to scroll by
+         private float startX = 0; //track x from one ACTION_MOVE to the next
+         private float startY = 0; //track y from one ACTION_MOVE to the next
+
+         public SampleView(Context context) {
+                 super(context);
+
+                 // Destination rect for our main canvas draw. It never changes.
+                 displayRect = new Rect(0, 0, displayWidth, displayHeight);
+                 // Scroll rect: this will be used to 'scroll around' over the
+                 // bitmap in memory. Initialize as above.
+                 scrollRect = new Rect(0, 0, displayWidth, displayHeight);
+
+                 // Load a large bitmap into an offscreen area of memory.
+                 bmLargeImage = BitmapFactory.decodeResource(getResources(),
+                         R.drawable.testlargeimg);
+         }
+        
+         @Override
+         public boolean onTouchEvent(MotionEvent event) {
+
+                 switch (event.getAction()) {
+                         case MotionEvent.ACTION_DOWN:
+                                 // Remember our initial down event location.
+                                 startX = event.getRawX();
+                                 startY = event.getRawY();
+                                 break;
+
+                         case MotionEvent.ACTION_MOVE:
+                                 float x = event.getRawX();
+                                 float y = event.getRawY();
+                                 // Calculate move update. This will happen many times
+                                 // during the course of a single movement gesture.
+                                 scrollByX = x - startX; //move update x increment
+                                 scrollByY = y - startY; //move update y increment
+                                 startX = x; //reset initial values to latest
+                                 startY = y;
+                                 invalidate(); //force a redraw
+                                 break;
+                 }
+                 return true; //done with this event so consume it
+         }
+
+         @Override
+         protected void onDraw(Canvas canvas) {
+
+                 // Our move updates are calculated in ACTION_MOVE in the opposite direction
+                 // from how we want to move the scroll rect. Think of this as dragging to
+                 // the left being the same as sliding the scroll rect to the right.
+                 int newScrollRectX = scrollRectX - (int)scrollByX;
+                 int newScrollRectY = scrollRectY - (int)scrollByY;
+
+                 // Don't scroll off the left or right edges of the bitmap.
+                 if (newScrollRectX < 0)
+                         newScrollRectX = 0;
+                 else if (newScrollRectX > (bmLargeImage.getWidth() - displayWidth))
+                         newScrollRectX = (bmLargeImage.getWidth() - displayWidth);
+
+                 // Don't scroll off the top or bottom edges of the bitmap.
+                 if (newScrollRectY < 0)
+                         newScrollRectY = 0;
+                 else if (newScrollRectY > (bmLargeImage.getHeight() - displayHeight))
+                         newScrollRectY = (bmLargeImage.getHeight() - displayHeight);
+
+                 // We have our updated scroll rect coordinates, set them and draw.
+                 scrollRect.set(newScrollRectX, newScrollRectY,
+                         newScrollRectX + displayWidth, newScrollRectY + displayHeight);
+                 Paint paint = new Paint();
+                 canvas.drawBitmap(bmLargeImage, scrollRect, displayRect, paint);
+
+                 // Reset current scroll coordinates to reflect the latest updates,
+                 // so we can repeat this update process.
+                 scrollRectX = newScrollRectX;
+                 scrollRectY = newScrollRectY;
+         }
+ }
+		
 		
 }
